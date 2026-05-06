92b88bedbb15196ac4b4fb2faf2cae72bfae274e
Abishek S <it22186942@my.sliit.lk>
2026-05-06 23:54:02 +0630

    user services status change back

--- user_service/index.js
+++ user_service/index.js
@@ -21,7 +21,7 @@ app.get('/users/:user_id', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-    res.json({ status: "healthy-ok" });
+    res.json({ status: "healthy" });
 });
 
 app.listen(port, () => {
